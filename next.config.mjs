00f3071e2e596023bfba034d894148a87b05f80e
Albert Tam <tamalbert8@gmail.com>
2024-02-29 06:20:38 -0500

    fix next.config a little

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  assetPrefix: isProd ? "/your-github-repo-name/" : "",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
