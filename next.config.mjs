982ae1432a62d98c33a9369aaf872fb2bd67eeb9
Albert Tam <tamalbert8@gmail.com>
2024-02-29 05:44:13 -0500

    ready for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
+
 const nextConfig = {
   reactStrictMode: true,
 };
